a604b83db96d567cc8d2a22e1b2c6079b569cc61
Jay Shah <sonjay97@gmail.com>
2025-03-29 17:03:49 -0500

    [Update][Styling] Added blog post file and styling

--- script.js
+++
@@ -1,6 +0,0 @@
-// Array to store blog posts
-const blogPosts = [
-    {
-        title: "Easy way to learn data structures, algorithms, and system design for FAANG+", date: "March 29, 2025",
-    }
-]
